262eee40576eb0931335e53c3e06ff434faf7bd5
Leonardo Mendes <leonardocm92@hotmail.com>
2017-10-19 20:00:58 -0200

    Criando profile de desenvolvimento

--- src/main/java/br/com/leonardo/cursomc/CursomcApplication.java
+++ src/main/java/br/com/leonardo/cursomc/CursomcApplication.java
@@ -1,35 +1,8 @@
 package br.com.leonardo.cursomc;
 
-import java.text.SimpleDateFormat;
-import java.util.Arrays;
-
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-import br.com.leonardo.cursomc.domain.Categoria;
-import br.com.leonardo.cursomc.domain.Cidade;
-import br.com.leonardo.cursomc.domain.Cliente;
-import br.com.leonardo.cursomc.domain.Endereco;
-import br.com.leonardo.cursomc.domain.Estado;
-import br.com.leonardo.cursomc.domain.ItemPedido;
-import br.com.leonardo.cursomc.domain.Pagamento;
-import br.com.leonardo.cursomc.domain.PagamentoBoleto;
-import br.com.leonardo.cursomc.domain.PagamentoCartao;
-import br.com.leonardo.cursomc.domain.Pedido;
-import br.com.leonardo.cursomc.domain.Produto;
-import br.com.leonardo.cursomc.domain.enums.EstadoPagamento;
-import br.com.leonardo.cursomc.domain.enums.TipoPessoa;
-import br.com.leonardo.cursomc.repositories.CategoriaRepository;
-import br.com.leonardo.cursomc.repositories.CidadeRepository;
-import br.com.leonardo.cursomc.repositories.ClienteRepository;
-import br.com.leonardo.cursomc.repositories.EnderecoRepository;
-import br.com.leonardo.cursomc.repositories.EstadoRepository;
-import br.com.leonardo.cursomc.repositories.ItemPedidoRepository;
-import br.com.leonardo.cursomc.repositories.PagamentoRepository;
-import br.com.leonardo.cursomc.repositories.PedidoRepository;
-import br.com.leonardo.cursomc.repositories.ProdutoRepository;
 
 @SpringBootApplication
 public class CursomcApplication  implements CommandLineRunner{ // A gente implementa essa interface para quando a aplicação iniciar ela ja instanciar um metodo para fazer o primeiro insert no banco para teste.
---
+++ src/main/java/br/com/leonardo/cursomc/config/DevConfig.java
@@ -0,0 +1,34 @@
+package br.com.leonardo.cursomc.config;
+
+import java.text.ParseException;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+import br.com.leonardo.cursomc.services.DBService;
+
+@Configuration
+@Profile("dev") // Com essa notação eu falo que todos os bins presentes aqui estão ativos quando no meu application.proprieis estiver com o test active
+public class DevConfig {
+	
+	@Autowired
+	private DBService dbtest;
+	
+	@Value("${spring.jpa.hibernate.ddl-auto}") // Essa notação eu consigo  validar se no application.proprie esta marcado para criar o banco novamente, somente assim poderemos iniciar o servico de preencer o banco
+	private String strategy;
+	
+	@Bean // Assim nos iremos start todo o servico do banco
+	public Boolean instantiateDataBase() throws ParseException {
+		
+		if(!"create".equalsIgnoreCase(strategy)) {
+			return false;
+		}
+		
+		dbtest.instantiateTestDataBase();		
+		return true;
+	}
+
+}
