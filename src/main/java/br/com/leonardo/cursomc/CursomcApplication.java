20817f28d5dbfdefe4fa59c4957f05d6b16ddce5
Leonardo Mendes <leonardocm92@hotmail.com>
2017-10-19 19:48:17 -0200

    Criando Profile de Teste

--- src/main/java/br/com/leonardo/cursomc/CursomcApplication.java
+++ src/main/java/br/com/leonardo/cursomc/CursomcApplication.java
@@ -33,25 +33,7 @@ import br.com.leonardo.cursomc.repositories.ProdutoRepository;
 
 @SpringBootApplication
 public class CursomcApplication  implements CommandLineRunner{ // A gente implementa essa interface para quando a aplicação iniciar ela ja instanciar um metodo para fazer o primeiro insert no banco para teste.
-	
-	@Autowired // Ja utilizamos anterior
-	private CategoriaRepository categoriarepository; 
-	@Autowired
-	private ProdutoRepository produtorepository;
-	@Autowired
-	private CidadeRepository cidaderepository;
-	@Autowired
-	private EstadoRepository estadorepository;
-	@Autowired
-	private ClienteRepository clienterepository;
-	@Autowired
-	private EnderecoRepository enderecorepository;
-	@Autowired
-	private PedidoRepository pedidorepository;
-	@Autowired
-	private PagamentoRepository pagamentorepository;
-	@Autowired
-	private ItemPedidoRepository itempedidorepository;
+
 	
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
@@ -59,103 +41,6 @@ public class CursomcApplication  implements CommandLineRunner{ // A gente implem
 
 	@Override
 	public void run(String... arg0) throws Exception { // Metodo implementado para fazer o save de dados no banco ao iniciar
-		Categoria cat1 = new Categoria(null,"Informatica");
-		Categoria cat2 = new Categoria(null,"Escritorio");
-		Categoria cat3 = new Categoria(null,"Casa Mesa e Banho");
-		Categoria cat4 = new Categoria(null,"Eletronicos");
-		Categoria cat5 = new Categoria(null,"Jardinagem");
-		Categoria cat6 = new Categoria(null,"Decoracao");
-		Categoria cat7 = new Categoria(null,"Tabacaria");
-		Categoria cat8 = new Categoria(null,"Perfumaria");
-		
-		Produto p1 = new Produto(null,"Computador", 2000.00);
-		Produto p2 = new Produto(null,"Impressora", 800.00);
-		Produto p3 = new Produto(null,"Mouse", 80.00);
-		Produto p4 = new Produto(null,"Mesa de Escritorio", 300.00);
-		Produto p5 = new Produto(null,"Toalha", 50.00);
-		Produto p6 = new Produto(null,"Colcha", 200.00);
-		Produto p7 = new Produto(null,"TV True Color", 1500.00);
-		Produto p8 = new Produto(null,"Roçadeira", 800.00);
-		Produto p9 = new Produto(null,"Abajour", 100.00);
-		Produto p10 = new Produto(null,"Pendente", 180.00);
-		Produto p11 = new Produto(null,"Shampoo", 90.00);
-		
-		cat1.getProdutos().addAll(Arrays.asList(p1,p2,p3));
-		cat2.getProdutos().addAll(Arrays.asList(p2, p4));
-		cat3.getProdutos().addAll(Arrays.asList(p5, p6));
-		cat4.getProdutos().addAll(Arrays.asList(p1, p2, p3, p7));
-		cat5.getProdutos().addAll(Arrays.asList(p8));
-		cat6.getProdutos().addAll(Arrays.asList(p9, p10));
-		cat7.getProdutos().addAll(Arrays.asList(p11));
-	
-		p1.getCategorias().addAll(Arrays.asList(cat1, cat4));
-		p2.getCategorias().addAll(Arrays.asList(cat1, cat2, cat4));
-		p3.getCategorias().addAll(Arrays.asList(cat1, cat4));
-		p4.getCategorias().addAll(Arrays.asList(cat2));
-		p5.getCategorias().addAll(Arrays.asList(cat3));
-		p6.getCategorias().addAll(Arrays.asList(cat3));
-		p7.getCategorias().addAll(Arrays.asList(cat4));
-		p8.getCategorias().addAll(Arrays.asList(cat5));
-		p9.getCategorias().addAll(Arrays.asList(cat6));
-		p10.getCategorias().addAll(Arrays.asList(cat6));
-		p11.getCategorias().addAll(Arrays.asList(cat7));
-		
-		categoriarepository.save(Arrays.asList(cat1,cat2,cat3,cat4,cat5,cat6,cat7,cat8)); // Como criamos o objeto do repository aqui vamos utilizar um de seus metodos ja herdados da Jpa
-		// Dentro do save utilizamos o Arrays.asList(cat1,cat2) que é um macete para salvar masi de um objeto ja no dB
-		
-		produtorepository.save(Arrays.asList(p1, p2, p3, p4, p5, p6, p7, p8, p9, p10, p11));
-
-		Estado est1 = new Estado(null, "Minas Gerais");
-		Estado est2 = new Estado(null, "São Paulo");
-		
-		Cidade c1 = new Cidade(null,"Uberlandia",est1);
-		Cidade c2 = new Cidade(null,"Sao Paulo",est2);
-		Cidade c3 = new Cidade(null,"Campinas",est2);
-		
-		est1.getCidades().addAll(Arrays.asList(c1));		
-		est2.getCidades().addAll(Arrays.asList(c2,c3));
-		
-		estadorepository.save(Arrays.asList(est1,est2));
-		cidaderepository.save(Arrays.asList(c1,c2,c3));
-		
-		Cliente cli1 = new Cliente(null, "Maria Silva", "maria@hotmail.com", "1234567", TipoPessoa.PESSOAFISICA);
-		cli1.getTelefones().addAll(Arrays.asList("123456789","987654321"));
-		
-		Endereco e1 = new Endereco(null, "Rua Flores", "300", "Jardim", "123465789", "Apto03", cli1, c1);
-		Endereco e2 = new Endereco(null, "Rua Rosas", "500", "Jardim 2", "987654321", "Fundos", cli1, c2);
-		
-		cli1.getEnderecos().addAll(Arrays.asList(e1,e2));
-		
-		clienterepository.save(Arrays.asList(cli1));
-		enderecorepository.save(Arrays.asList(e1,e2));
-		
-		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy hh:mm"); // Classe que ira formatar a data
-		Pedido ped1 = new Pedido(null, sdf.parse("30/09/2017 10:32"), cli1, e1);
-		Pedido ped2 = new Pedido(null, sdf.parse("10/10/2017 14:45"), cli1, e2);
-		
-		Pagamento pagto1 = new PagamentoCartao(null, EstadoPagamento.PAGO, ped1, 6);
-		ped1.setPagamento(pagto1);
-		
-		Pagamento pagto2 = new PagamentoBoleto(null, EstadoPagamento.PENDENTE, ped2, sdf.parse("10/11/2017 14:45"), null);
-		ped2.setPagamento(pagto2);
-		
-		cli1.getPedidos().addAll(Arrays.asList(ped1, ped2));
-		
-		pedidorepository.save(Arrays.asList(ped1,ped2));
-		pagamentorepository.save(Arrays.asList(pagto1,pagto2));
-		
-		ItemPedido ip1 = new ItemPedido(p1, ped1, 0.00, 1, 2000.00);
-		ItemPedido ip2 = new ItemPedido(p3, ped1, 0.00, 2, 80.00);
-		ItemPedido ip3 = new ItemPedido(p2, ped2, 100.00, 2, 800.00);
-		
-		ped1.getItens().addAll(Arrays.asList(ip1,ip2));
-		ped2.getItens().addAll(Arrays.asList(ip3));
-		
-		p1.getItens().addAll(Arrays.asList(ip1));
-		p2.getItens().addAll(Arrays.asList(ip3));
-		p3.getItens().addAll(Arrays.asList(ip2));
-		
-		itempedidorepository.save(Arrays.asList(ip1,ip2,ip3));
-	
+			
 	}
 }
---
+++ src/main/java/br/com/leonardo/cursomc/config/TestConfig.java
@@ -0,0 +1,25 @@
+package br.com.leonardo.cursomc.config;
+
+import java.text.ParseException;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+import br.com.leonardo.cursomc.services.DBService;
+
+@Configuration
+@Profile("test") // Com essa notação eu falo que todos os bins presentes aqui estão ativos quando no meu application.proprieis estiver com o test active
+public class TestConfig {
+	
+	@Autowired
+	private DBService dbtest;
+	
+	@Bean // Assim nos iremos start todo o servico do banco
+	public Boolean instantiateDataBase() throws ParseException {
+		dbtest.instantiateTestDataBase();		
+		return true;
+	}
+
+}
